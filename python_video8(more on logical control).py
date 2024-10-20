76d5a78ca4ca7f14377c8e76c9cb4c9a35e66a6a
Yikealo Asgodom Abraha <yikealo255@gmail.com>
2024-10-20 16:57:13 +0200

    8.Video

---
+++ python_video8(more on logical control).py
@@ -0,0 +1,28 @@
+score=70
+
+if score>=90:
+    print("Grade:A")
+
+elif score>=80:
+    print("Grade:B")
+
+elif score>=70:
+    print("Grade:C")
+elif score>=60:
+    print("Grade:D")
+else:
+    print("Fail")
+
+
+
+tree = "banana"
+
+match score:
+ case "banana":
+  print("it is fruit")
+
+ case "tomato":
+   print("it is a begi")
+
+ case_:
+   print("it is just a tree")
